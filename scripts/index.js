74f96c692a5d56a5b2bfc5933c6ef9c4c59f5f88
elgynart <elgynart@gmail.com>
2022-05-03 21:58:27 +0300

    Added two last information pages

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,9 @@ const form = document.querySelector('form.poll');
 const button = document.querySelector('.submit');
 const fields = document.querySelectorAll('fieldset');
 const error = document.querySelector('.error');
+const questionsBlock = document.querySelector('.questions');
+const statisticsBlock = document.querySelector('.statistics');
+const summaryBlock = document.querySelector('.summary');
 
 form.addEventListener('change', formChange);
 button.addEventListener('click', nextForm);
@@ -12,9 +15,9 @@ button.addEventListener('click', nextForm);
 function formChange() {
     button.classList.add('active');
 
-    if(error.classList.contains('active')) {
-        error.classList.remove('active');
-    }
+    // if(error.classList.contains('active')) {
+    //     error.classList.remove('active');
+    // }
 
     window.scrollTo({
         top: button.getBoundingClientRect().top,
@@ -53,11 +56,92 @@ function nextForm(e) {
 
     if(prevActiveField + 1 <= fields.length - 1) {
         fields[prevActiveField + 1].classList.add('active');
-        this.classList.remove('active');
+        button.classList.remove('active');
     }else {
         const formData = new FormData(form);
         for(let el of formData) {
             console.log(el);
-        } 
+        }
+
+        questionsBlock.classList.remove('active');
+        statistics();
+    }
+}
+
+function statistics() {
+    statisticsBlock.classList.add('active');
+
+    const statisticsProgress = document.querySelector('.statistics-progress');
+    const statisticsList = statisticsBlock.querySelectorAll('ul > li');
+    const statisticsButton = document.querySelector('.statistics-button');
+    const heads = statisticsBlock.querySelectorAll('h4');
+    const [ , tapeMessage, doneMessage] = heads;
+    
+    const statsWidth = statisticsProgress.parentElement.clientWidth;
+    statisticsButton.addEventListener('click', showSummary);
+    
+    async function showStatistics() {
+        let i = 0;
+        for(const item of statisticsList) {
+            await nextItem(item, i);
+            i = i + 1;
+        }
+
+        tapeMessage.classList.add('show');
+        doneMessage.classList.add('show');
+        statisticsButton.classList.add('active');
+
+        window.scrollTo({
+            top: statisticsButton.getBoundingClientRect().top,
+            behavior: 'smooth'
+        });
+    }
+    showStatistics();
+
+    function progressStats(ind) {
+        let widthStatsPercent = Math.round((statsWidth / statisticsList.length) / statsWidth * 100) * (ind + 1);
+        if(ind === statisticsList.length - 1) widthStatsPercent = 100;
+        
+        statisticsProgress.style.cssText = `width: ${widthStatsPercent}%; transition: width 1s ease`;
+        statisticsProgress.innerText = widthStatsPercent + '%';
+    }
+
+    function nextItem(item, ind) {
+        return new Promise(resolve => setTimeout(() => {
+                item.classList.add('show');
+                progressStats(ind);
+                resolve();
+            }, 300)
+        );
+    }
+
+    function showSummary() {
+        statisticsBlock.classList.remove('active');
+        summary();
+    }
+}
+
+function summary() {
+    summaryBlock.classList.add('active');
+
+    const summaryButton = document.querySelector('.summary-button');
+    summaryButton.addEventListener('click', getDataFromSever);
+
+    function getDataFromSever() {
+        const url = "https://swapi.dev/api/people/1/";
+        const opts = {
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+        };
+
+        fetch(url, opts)
+            .then(res => res.json())
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 }
